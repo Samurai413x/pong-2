f9b828bedb10b3a1a7193095cd96d4596861b3af
Alice He <hosaki@live.jp>
2013-01-21 01:29:28 -0800

    Created "base"
    Just a simple base.

--- pong.py
+++ pong.py
@@ -1,1 +1,12 @@
-print ("PONG PONG POOOOOOOONG")
+import pygame, sys
+from pygame.locals import*
+
+pygame.init()
+DISPLAYSURF = pygame.display.set_mode((600, 450))
+pygame.display.set_caption('Pong!')
+while True: #game loop
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
+    pygame.display.update()
